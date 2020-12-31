d4ad8e23e58c27266dd89b716e218ecfb094d642
Hidemitsu Aoki <olympos0117@gmail.com>
2020-12-31 15:52:10 +0900

    Use grid component

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,37 +1,26 @@
-import React, { useState } from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import React from "react";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Container from "@material-ui/core/Container";
+import { Grid } from "@material-ui/core";
 
 interface Props {}
 
 const App: React.FC<Props> = (props) => {
-  const [count, setCount] = useState<number>(0);
-
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        {count}
-        <a
-          onClick={() => {
-            setCount(count + 1);
-          }}
-        >
-          add
-        </a>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container component="main" maxWidth="xl">
+      <CssBaseline />
+      <Grid container spacing={3}>
+        <Grid item xs={3}>
+          bus
+        </Grid>
+        <Grid item xs={6}>
+          main
+        </Grid>
+        <Grid item xs={3}>
+          user
+        </Grid>
+      </Grid>
+    </Container>
   );
 };
 
