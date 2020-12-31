fde8a86f166e5e78f4e32c56ab83be4a8d178c31
Hidemitsu Aoki <olympos0117@gmail.com>
2020-12-31 16:34:39 +0900

    Introduce List component from material-ui

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,17 +1,53 @@
 import React from "react";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import Container from "@material-ui/core/Container";
-import { Grid } from "@material-ui/core";
+import {
+  Grid,
+  List,
+  ListItem,
+  ListItemText,
+  ListSubheader,
+  makeStyles,
+} from "@material-ui/core";
+import { red } from "@material-ui/core/colors";
+import SendIcon from "@material-ui/icons/Send";
 
 interface Props {}
 
+const useStyles = makeStyles((theme) => ({
+  red: {
+    // marginTop: theme.spacing(8),
+    // display: "flex",
+    // flexDirection: "column",
+    // alignItems: "center",
+    backgroundColor: red[500],
+  },
+}));
+
 const App: React.FC<Props> = (props) => {
+  const classes = useStyles();
   return (
     <Container component="main" maxWidth="xl">
       <CssBaseline />
       <Grid container spacing={3}>
-        <Grid item xs={3}>
-          bus
+        <Grid item xs={3} className={classes.red}>
+          <List
+            component="nav"
+            aria-labelledby="nested-list-subheader"
+            subheader={
+              <ListSubheader component="div" id="nested-list-subheader">
+                <SendIcon />
+                hitchhikers
+              </ListSubheader>
+            }
+          >
+            <ListItem button>
+              <ListItemText primary="username" />
+            </ListItem>
+            <ListItem button>
+              <ListItemText primary="Drafts" />
+            </ListItem>
+          </List>
         </Grid>
         <Grid item xs={6}>
           main
